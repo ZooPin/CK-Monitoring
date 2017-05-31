43bc360933946cdfc7d761133fc8b0f9725f5866
Jilanow <fabulet@intechinfo.fr>
2017-05-31 08:42:19 +0200

    update the index log method

--- Server/Lucene/LuceneIndexer.cs
+++ Server/Lucene/LuceneIndexer.cs
@@ -8,6 +8,7 @@ using Lucene.Net.Documents;
 using Lucene.Net.Analysis;
 using Lucene.Net.Util;
 using Lucene.Net.Analysis.Standard;
+using CK.Monitoring;
 
 
 namespace GloutonLucene
@@ -49,25 +50,40 @@ namespace GloutonLucene
         {
             Document document = new Document();
 
-            Field logType = new TextField("LogType", log.LogType, Field.Store.YES);
-            Field logLevel = new TextField("LogLevel", log.LogLevel, Field.Store.YES);
-            Field text = new TextField("Text", log.Text, Field.Store.YES);
-            Field tags = new TextField("Tags", log.Tags, Field.Store.YES);
-            Field logTime = new TextField("LogTime", log.LogTime, Field.Store.YES);
-            Field exception = new TextField("Exception", log.Exception, Field.Store.YES);
-            Field fileName = new TextField("FileName", log.FileName, Field.Store.YES);
-            Field lineNumber = new TextField("LineNumber", log.LineNumber, Field.Store.YES);
-            Field conclusions = new TextField("Conclusions", log.Conclusions, Field.Store.YES);
+            if (log.LogType == LogEntryType.Line || log.LogType == LogEntryType.OpenGroup)
+            {
+                Field logLevel = new TextField("LogLevel", log.LogLevel.ToString(), Field.Store.YES);
+                Field text = new TextField("Text", log.Text, Field.Store.YES);
+                Field tags = new TextField("Tags", log.Tags.ToString(), Field.Store.YES);
+                Field logTime = new TextField("LogTime", log.LogTime.TimeUtc.ToString(), Field.Store.YES);
+                Field fileName = new TextField("FileName", log.FileName, Field.Store.YES);
+                Field lineNumber = new TextField("LineNumber", log.LineNumber.ToString(), Field.Store.YES);
+                if (log.Exception != null)
+                {
+                    Field exception = new TextField("Exception", log.Exception.Message, Field.Store.YES);
+                    document.Add(exception);
+                }
+                document.Add(logLevel);
+                document.Add(text);
+                document.Add(tags);
+                document.Add(logTime);
+                document.Add(fileName);
+                document.Add(lineNumber);
+            }
+
+            else if (log.LogType == LogEntryType.CloseGroup)
+            {
+                Field logLevel = new TextField("LogLevel", log.LogLevel.ToString(), Field.Store.YES);
+                Field conclusions = new TextField("Conclusions", log.Conclusions.ToString(), Field.Store.YES);
+                Field logTime = new TextField("LogTime", log.LogTime.TimeUtc.ToString(), Field.Store.YES);
+
+                document.Add(logLevel);
+                document.Add(logTime);
+            }
+
+            Field logType = new TextField("LogType", log.LogType.ToString(), Field.Store.YES);
 
             document.Add(logType);
-            document.Add(logLevel);
-            document.Add(text);
-            document.Add(tags);
-            document.Add(logTime);
-            document.Add(exception);
-            document.Add(fileName);
-            document.Add(lineNumber);
-            document.Add(conclusions);
 
             return document;
         }
