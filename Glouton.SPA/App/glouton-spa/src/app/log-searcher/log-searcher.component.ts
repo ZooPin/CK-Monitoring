42d1324d5ee9d88bb47d73b4444ac9efe91e4982
ZooPin <battle.raibleur@gmail.com>
2017-06-19 13:52:00 +0200

    Update View Model with the update of the indexer

--- Glouton.SPA/App/glouton-spa/src/app/log-searcher/log-searcher.component.ts
+++ Glouton.SPA/App/glouton-spa/src/app/log-searcher/log-searcher.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LogSearcherService } from '../log-searcher.service';
-import { LogType } from '../class/ILogView';
+import { LogType, ILogView } from '../class/ILogView';
 
 @Component({
   selector: 'app-log-searcher',
@@ -9,7 +9,7 @@ import { LogType } from '../class/ILogView';
 })
 export class LogSearcherComponent implements OnInit {
 
-  data : any;
+  data : Array<ILogView>;
 
   constructor(private LogSearcherService: LogSearcherService) { }
 
@@ -17,6 +17,6 @@ export class LogSearcherComponent implements OnInit {
   }
 
   async Search(query:string) {
-    await this.LogSearcherService.Search(query).subscribe(data => this.data = data);
+    await this.LogSearcherService.Search(query).subscribe(data => this.data = data as Array<ILogView>);
   }
 }
