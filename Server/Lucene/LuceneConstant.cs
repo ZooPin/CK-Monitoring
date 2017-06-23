acdddd3553788ff0b58622f51ebb9a0fcd467cbc
ZooPin <battle.raibleur@gmail.com>
2017-06-23 13:57:25 +0200

    update path of indexer

--- Server/Lucene/LuceneConstant.cs
+++ Server/Lucene/LuceneConstant.cs
@@ -23,7 +23,7 @@ namespace GloutonLucene
                 System.IO.Directory.CreateDirectory(path);
             return path;
 #else
-            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Glouton", "Logs");
+            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Glouton", "Logs");
             if (!Directory.Exists(path))
                 Directory.CreateDirectory(path);
             return path;
